88a4b925e1c493744db910b0e4580a4d04a41d95
Reina Wardhani <65999844+reinadhn@users.noreply.github.com>
2025-06-02 21:56:20 +0700

    Add files via upload

---
+++ Random Numbers/main.py
@@ -0,0 +1,13 @@
+import random
+
+N_NUMBERS = 10
+MIN_VALUE = 1
+MAX_VALUE = 100
+
+def main():
+    for i in range(10):
+        print(random.randint(1,100))
+    pass
+
+if __name__ == '__main__':
+    main()
